4c532cde3c119a9f9491f4dbb6ef101eaddd9fc7
KodamaHiroaki <kdm@KodamaHiroakinoMacBook-Air.local>
2020-04-12 21:07:33 +0900

    FinishBookers1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,12 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
   	if @book.save
-  	flash[:notice] = 'Book was successfully created.'
+    flash[:notice] = "Book was successfully created."
   	redirect_to book_path(@book.id)
     else
-    flash.now[:alert] = ''
-    render ""
-	end
+    @books = Book.all
+    render :index
+	  end
   end
 
   def show
@@ -25,15 +25,20 @@ class BooksController < ApplicationController
 
   def update
   	@book = Book.find(params[:id])
-  	@book.update(book_params)
-  	redirect_to book_path(@book.id)
+  	if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+  	  redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
   	book = Book.find(params[:id])
   	if book.destroy
-  		flash[:notice] = 'Book was successfully destroyed.'
-  	redirect_to books_path
+      flash[:notice] = "Book was successfully destroyed."
+  	  redirect_to books_path
+    end
   end
 
   private
